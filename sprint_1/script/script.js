2590708ab7bb6f35fa6fa352ef6cc10538cac3a6
Marco Pineda <pinedamarcoantonio3@gmail.com>
2021-10-26 17:52:58 -0500

    sprnt avance js

---
+++ sprint_1/script/script.js
@@ -0,0 +1,18 @@
+let pcvalue = 820000
+let form = document.getElementById('form')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+})
+
+function numTotal() {
+    let npc = Number(document.getElementById('pcnumber'))
+    let numPc = ["", "true"]
+    numPc[0] = npc
+    if (numpc = true) {
+        let test = 123
+        return test
+    }
+    console.log(pcvalue, numPc, test)
+}
