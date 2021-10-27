fbc9cf2da67c6d3061271e4c3f729558c794e3da
Marco Pineda <pinedamarcoantonio3@gmail.com>
2021-10-27 15:02:59 -0500

    sprint 1 finalizado

--- docs/script/script.js
+++ docs/script/script.js
@@ -1,18 +1,44 @@
-let pcvalue = 820000
 let form = document.getElementById('form')
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    
+    operacion()
 })
 
-function numTotal() {
-    let npc = Number(document.getElementById('pcnumber'))
-    let numPc = ["", "true"]
-    numPc[0] = npc
-    if (numpc = true) {
-        let test = 123
-        return test
+function operacion() {
+    let numpc = Number(document.getElementById('pcnumber').value)
+    let totalCompra = 0
+    let discount = {}
+    let pcvalue = 820000
+    let discDisplay = document.getElementById('discDisplay')
+    let discount2
+
+    totalCompra = pcvalue * numpc
+
+    if (3280001 >= totalCompra && totalCompra >= 1639999) {
+        discount[1] = 0.15
+        discount[0] = "15%"
+    } else if (6560000 >= totalCompra && totalCompra > 3280000) {
+        discount[1] = 0.25
+        discount[0] = "25%"
+    } else if (9840000 >= totalCompra && totalCompra > 6560000) {
+        discount[1] = 0.35
+        discount[0] = "35%"
+    } else {
+        discount[1] = 0
+        discount[0] = "0%"
     }
-    console.log(pcvalue, numPc, test)
+
+let numDiscount = totalCompra * discount[1]
+    totaldescuento = totalCompra - numDiscount
+
+    document.getElementById('pcCompra').value = totalCompra
+    document.getElementById('pcDesc').value = numDiscount
+    document.getElementById('pcTotal').value = totaldescuento
+
+    discDisplay.innerHTML = `
+    <p>Tu descuento fue del ${discount[0]}</p>
+    `;
+
+    console.log(pcvalue, numpc, totalCompra, discount[1], totaldescuento, discount[0])
 }
