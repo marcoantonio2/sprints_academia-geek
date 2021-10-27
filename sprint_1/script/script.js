e79631774b70d30310e3a14420e4badcff0211a1
Marco Pineda <pinedamarcoantonio3@gmail.com>
2021-10-27 15:16:13 -0500

    comentarios explicando todo

--- sprint_1/script/script.js
+++ sprint_1/script/script.js
@@ -1,20 +1,21 @@
 let form = document.getElementById('form')
-
+//escuchar el evento submit
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     operacion()
 })
-
+// funcion para hacer todo
 function operacion() {
+    // se consulta por  el numero de pcs al igual que se crean las variables
     let numpc = Number(document.getElementById('pcnumber').value)
     let totalCompra = 0
     let discount = {}
     let pcvalue = 820000
     let discDisplay = document.getElementById('discDisplay')
     let discount2
-
+    // se saca el total a pagar a partir del numero de pcs
     totalCompra = pcvalue * numpc
-
+    // se busca cuanto es el descuento dependiendo del total
     if (3280001 >= totalCompra && totalCompra >= 1639999) {
         discount[1] = 0.15
         discount[0] = "15%"
@@ -29,16 +30,16 @@ function operacion() {
         discount[0] = "0%"
     }
 
-let numDiscount = totalCompra * discount[1]
-    totaldescuento = totalCompra - numDiscount
+    //se saca el número a restar para descuento
 
+    let numDiscount = totalCompra * discount[1]
+    totaldescuento = totalCompra - numDiscount
+    // se pone los elementos en los input
     document.getElementById('pcCompra').value = totalCompra
     document.getElementById('pcDesc').value = numDiscount
     document.getElementById('pcTotal').value = totaldescuento
-
+    // se informa cuanto descuento se hiza
     discDisplay.innerHTML = `
     <p>Tu descuento fue del ${discount[0]}</p>
     `;
-
-    console.log(pcvalue, numpc, totalCompra, discount[1], totaldescuento, discount[0])
 }
